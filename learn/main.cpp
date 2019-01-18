fded3916404a4cd3089d474b6e315e664f1a2cb5
wang-xin-xin <wang_xin_xin@outlook.com>
2019-01-18 17:52:18 +0700

    init

---
+++ learn/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "learn.h"
+
+int main(int argc, char *argv[]){
+  int x1 = 10;
+  int x2 = 20;
+  std::cout << "Hello World!" << add(x1, x2) << std::endl;
+
+  printSize();
+  printInfNan();
+  printBool();
+  printChar();
+  printBitwise();
+  std::cout << getRandomNumber() << std::endl;
+  std::cout << getRandomNumber(1, 6) << std::endl;
+  std::cout << getRandom() << std::endl;
+  printVector();
+  
+  return 0;
+}
